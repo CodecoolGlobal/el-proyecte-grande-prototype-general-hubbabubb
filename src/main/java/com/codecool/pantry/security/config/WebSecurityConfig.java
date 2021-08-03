302be572ffe9c870d47b0193000f6949106dbfb3
Souf1an3 <irbouhsoufiane@gmail.com>
2021-08-03 14:50:48 +0200

    Grocery list repo,service,entity,controller created for base level, details tomorrow

--- src/main/java/com/codecool/pantry/security/config/WebSecurityConfig.java
+++ src/main/java/com/codecool/pantry/security/config/WebSecurityConfig.java
@@ -22,15 +22,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //WARN: Should be enabled in production
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("api/v*/registration/**")
-                    .permitAll()
+                .antMatchers("/**")
+                .permitAll()
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin();
+                .authenticated().and()
+                .formLogin()
+                .defaultSuccessUrl("/test")
+                .permitAll();
+//        http
+//                //WARN: Should be enabled in production
+//                .csrf().disable()
+//                .authorizeRequests()
+//                    .antMatchers("api/v*/registration/**")
+//                    .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin();
     }
 
     @Override
