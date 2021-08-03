d2c0370fbaabf6b57752d547d38aa172b2833bcc
smarcell91 <smarcell91@gmail.com>
2021-08-03 14:27:04 +0200

    Recipe controller

--- src/main/java/com/codecool/pantry/contoller/recipe/RecipeController.java
+++ src/main/java/com/codecool/pantry/contoller/recipe/RecipeController.java
@@ -4,20 +4,22 @@ package com.codecool.pantry.contoller.recipe;
 import com.codecool.pantry.entity.recipe.Recipe;
 import com.codecool.pantry.repository.appuser.RecipeRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(path = "api/v1/recipe")
+@RequestMapping(path = "/api/v1/recipe")
 public class RecipeController {
 
     private final RecipeRepository recipeRepository;
 
-    @GetMapping
-    public String getRecipe(@RequestBody Recipe recipe) {
-        return null;
+    @PostMapping
+    public void createClient(@RequestBody Recipe recipe) {
+        recipeRepository.save(recipe);
+    }
+
+    @GetMapping("/{id}")
+    public Recipe getRecipe(@PathVariable Long id) {
+        return recipeRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 }
