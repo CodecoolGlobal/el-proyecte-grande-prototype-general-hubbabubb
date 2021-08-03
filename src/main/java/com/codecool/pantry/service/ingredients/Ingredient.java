fe39468c455b013053f6ac14cbdfec58513a74a2
Zanghetsu <boythasx@gmail.com>
2021-08-03 17:14:14 +0200

    Added constructor and getters for Ingredient class' fields

--- src/main/java/com/codecool/pantry/service/ingredients/Ingredient.java
+++ src/main/java/com/codecool/pantry/service/ingredients/Ingredient.java
@@ -12,4 +12,27 @@ public class Ingredient{
     private String originalName;
     private String shoppingListUnits;
     private Set<String> cookingListUnits;
+
+    public Ingredient(Integer id, String originalName, String shoppingListUnits, Set<String> cookingListUnits) {
+        this.id = id;
+        this.originalName = originalName;
+        this.shoppingListUnits = shoppingListUnits;
+        this.cookingListUnits = cookingListUnits;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public String getShoppingListUnits() {
+        return shoppingListUnits;
+    }
+
+    public Set<String> getCookingListUnits() {
+        return cookingListUnits;
+    }
 }
