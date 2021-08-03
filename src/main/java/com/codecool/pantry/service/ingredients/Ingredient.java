a2d02f373050bf392d0e282dfdc5f13b4bc911e7
Zanghetsu <boythasx@gmail.com>
2021-08-03 17:15:10 +0200

    Removed redundant code from Ingredient class

--- src/main/java/com/codecool/pantry/service/ingredients/Ingredient.java
+++ src/main/java/com/codecool/pantry/service/ingredients/Ingredient.java
@@ -1,10 +1,5 @@
 package com.codecool.pantry.service.ingredients;
 
-import com.codecool.pantry.repository.ingredients.IngredientCsvReader;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.Set;
 
 public class Ingredient{
