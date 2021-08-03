6eb6b4101bf03d02e3faaac866f303fe1263dbfb
Zanghetsu <boythasx@gmail.com>
2021-08-03 13:53:30 +0200

    Interface - default method reads csv

--- src/main/java/com/codecool/pantry/Main.java
+++ src/main/java/com/codecool/pantry/Main.java
@@ -1,12 +1,19 @@
 package com.codecool.pantry;
 
+import com.codecool.pantry.service.ingredients.Ingredient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+
+		Ingredient ing = new Ingredient();
+		ing.printMap();
 		SpringApplication.run(Main.class, args);
 	}
 
