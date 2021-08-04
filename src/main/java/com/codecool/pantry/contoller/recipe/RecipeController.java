22d962d55ad0261a2e03fa256c54da56dd01c02b
smarcell91 <smarcell91@gmail.com>
2021-08-04 13:05:36 +0200

    recipe API from spoonacular by name and by id

--- src/main/java/com/codecool/pantry/contoller/recipe/RecipeController.java
+++ src/main/java/com/codecool/pantry/contoller/recipe/RecipeController.java
@@ -4,11 +4,17 @@ package com.codecool.pantry.contoller.recipe;
 import com.codecool.pantry.entity.recipe.Recipe;
 import com.codecool.pantry.repository.appuser.RecipeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(path = "/api/v1/recipe")
 public class RecipeController {
 
     private final RecipeRepository recipeRepository;
@@ -18,8 +24,19 @@ public class RecipeController {
         recipeRepository.save(recipe);
     }
 
-    @GetMapping("/{id}")
-    public Recipe getRecipe(@PathVariable Long id) {
-        return recipeRepository.findById(id).orElseThrow(RuntimeException::new);
+    @GetMapping("api/v1/recipe/search/{name}")
+    public ResponseEntity<String> searchRecipe(@PathVariable(value = "name") String name) {
+        final String uri = String.format("https://api.spoonacular.com/recipes/complexSearch?query=%s&number=25&apiKey=8dc3ef2ffcf54e6781629ee83623d725", name);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("api/v1/recipe/{id}")
+    public ResponseEntity<String> test(@PathVariable(value = "id") Long id) {
+        final String uri = String.format("https://api.spoonacular.com/recipes/%s/information?apiKey=8dc3ef2ffcf54e6781629ee83623d725", id);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        return result;
     }
 }
