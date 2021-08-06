1ffe39fcf21453a2e57c3c8fc7e0857a93a4cebb
Daniel Tokai <tokaidaniel@gmail.com>
2021-08-06 14:22:39 +0200

    pantry id generation

--- src/main/java/com/codecool/pantry/entity/Pantry.java
+++ src/main/java/com/codecool/pantry/entity/Pantry.java
@@ -1,20 +1,37 @@
 package com.codecool.pantry.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.codecool.pantry.entity.appuser.AppUser;
+import com.codecool.pantry.service.ingredients.Ingredient;
 
-@Entity
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Entity(name = "Pantry")
 public class Pantry {
-    private @Id
-    @GeneratedValue
-    Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Id
+    @SequenceGenerator(
+            name = "pantry_sequence",
+            sequenceName = "pantry_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "pantry_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private  Long id;
+    private String name;
+    @OneToMany
+    private Set<AppUser> users = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+    //TODO: Make Ingredients class immutable for HashMap usage
+    //@OneToMany(mappedBy = "pantry")
+    //private Map<Ingredient, Integer> ingredientQuantity = new HashMap<>();
 }
