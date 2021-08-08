b0cc01484f30c4d55083930d4e5a5848da610d37
Huber Gergő <71314614+hubbabubb@users.noreply.github.com>
2021-08-08 10:51:08 +0200

    Modify basic index.js, index,css and App related files Adding new package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Layout from "./components/layout/Layout";
+
+import './App.module.css';
+import Favorites from "./pages/Favorites";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <Home />
+                </Route>
+                <Route path='/register'>
+                    <Register/>
+                </Route>
+                <Route path='/favorites'>
+                    <Favorites/>
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
