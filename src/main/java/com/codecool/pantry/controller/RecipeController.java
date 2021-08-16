3ff6ffb118ff2fbfe92c35409f1d5a078c0b2bdc
Souf1an3 <irbouhsoufiane@gmail.com>
2021-08-16 13:34:00 +0200

    recipe apis added

--- src/main/java/com/codecool/pantry/controller/RecipeController.java
+++ src/main/java/com/codecool/pantry/controller/RecipeController.java
@@ -1,14 +1,16 @@
 package com.codecool.pantry.controller;
 
 
+import lombok.AllArgsConstructor;
 import com.codecool.pantry.entity.recipe.Recipe;
 import com.codecool.pantry.repository.recipe.RecipeRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(path = "/api/v1/recipe")
 public class RecipeController {
 
     private final RecipeRepository recipeRepository;
@@ -18,8 +20,20 @@ public class RecipeController {
         recipeRepository.save(recipe);
     }
 
-    @GetMapping("/{id}")
-    public Recipe getRecipe(@PathVariable Long id) {
-        return recipeRepository.findById(id).orElseThrow(RuntimeException::new);
+    // search recipe by name route
+    @GetMapping("api/v1/recipe/search/{name}")
+    public ResponseEntity<String> searchRecipeByName(@PathVariable(value = "name") String name) {
+        final String uri = String.format("https://api.spoonacular.com/recipes/complexSearch?query=%s&number=25&apiKey=8dc3ef2ffcf54e6781629ee83623d725", name);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(uri, String.class);
+    }
+
+
+    // get recipe by id
+    @GetMapping("api/v1/recipe/{id}")
+    public ResponseEntity<String> getRecipeById(@PathVariable(value = "id") Long id) {
+        final String uri = String.format("https://api.spoonacular.com/recipes/%s/information?apiKey=8dc3ef2ffcf54e6781629ee83623d725", id);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(uri, String.class);
     }
 }
