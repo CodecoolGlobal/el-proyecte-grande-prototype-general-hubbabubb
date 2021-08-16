f0530048bb43eb2c979eca796f793a56ee66601d
Daniel Tokai <tokaidaniel@gmail.com>
2021-08-16 10:40:15 +0200

    Pantry repository deleted

--- src/main/java/com/codecool/pantry/repository/PantryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.pantry.repository;
-
-import com.codecool.pantry.entity.Pantry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PantryRepository extends JpaRepository<Pantry, Long> {
-}
