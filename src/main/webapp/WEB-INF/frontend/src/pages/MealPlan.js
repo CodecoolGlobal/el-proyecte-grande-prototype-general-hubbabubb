9755d05cfb015d6b3213abe7d7952348da50eb1f
smarcell91 <smarcell91@gmail.com>
2021-08-18 22:31:29 +0200

    react meal plan with buttons

--- src/main/webapp/WEB-INF/frontend/src/pages/MealPlan.js
+++ src/main/webapp/WEB-INF/frontend/src/pages/MealPlan.js
@@ -1,6 +1,11 @@
-import {ListItem} from "@material-ui/core";
+import {Checkbox, FormControlLabel, Grid} from "@material-ui/core";
+import {Favorite, FavoriteBorder} from "@material-ui/icons";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 
 function MealPlan() {
+    const userId = 6;
+
     const data = [
         {
             id: 1,
@@ -69,7 +74,7 @@ function MealPlan() {
                 glutenFree: false,
                 cheap: true,
                 dairyFree: false,
-                healthScore: 5,
+                healthScore: 4,
                 instructions: "pizza bla-bla"
             },
             date: "2021-11-11",
@@ -116,34 +121,60 @@ function MealPlan() {
         }
     ];
 
-    function recipeInfo(recipe) {
-        return (
-            <div>
-                {recipe.title}  |
-                {recipe.image}  |
-                {recipe.healthScore}  |
-                {}
-            </div>
-        );
-    }
 
     function listMealPlans() {
         return (
             <div>
                 {data.map(plan => {
                     return (
-                        <div>
-                            {plan.id}  |
-                            {plan.date}  |
-                            <div>
-                                {recipeInfo(plan.recipe)}
-                            </div>
+                        <div style={{display: 'flex',  justifyContent:'center', height:'15vh'}}>
+                            <Grid direction={"column"}
+                            style={{border: '1px solid grey', width: '400px'}}>
+                                <Grid direction={"column"}
+                                      justifyContent={"center"}>
+                                    <div style={{display: 'flex',  justifyContent:'center'}}>{plan.recipe.title}</div>
+                                    <div style={{display: 'flex',  justifyContent:'center'}}>{plan.recipe.image}</div>
+                                    <div style={{display: 'flex',  justifyContent:'center'}}>Health score: {plan.recipe.healthScore}</div>
+                                </Grid>
+                                <div style={{display: 'flex',  justifyContent:'center'}}>
+                                    <span>
+                                        <FormControlLabel
+                                            control={<Checkbox icon={<FavoriteBorder />}
+                                                               checkedIcon={<Favorite />}
+                                                               onClick={sendLike(plan.id)}
+                                                               name="checkedLike" />}
+                                            label=""
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox icon={<DeleteIcon />}
+                                                               checkedIcon={<DeleteIcon />}
+                                                               onClick={sendDislike(plan.id)}
+                                                               name="checkedLike" />}
+                                            label=""
+                                        />
+                                    </span>
+                                </div>
+                                <div>
+                                    {plan.date}
+                                </div>
+                            </Grid>
 
                         </div>
                     );
                 })}
             </div>
         );
+    }
+
+
+    function sendLike(mealPlanId) {
+        fetch(`http://localhost:8080/api/v1/meal-plan/dislike/${mealPlanId}/${userId}`)
+            .then(() => {console.log("LikeSent")});
+    }
+
+    function sendDislike(mealPlanId) {
+        fetch(`http://localhost:8080/api/v1/meal-plan/dislike/${mealPlanId}/${userId}`)
+            .then(() => {console.log("DislikeSent")});
     }
 
     return (
